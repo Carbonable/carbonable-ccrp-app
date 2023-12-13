92f111fc524fb4667ab8c7db23a8a801b9219fe5
François Schuers <francois.schuers@gmail.com>
2023-12-13 16:27:22 +0100

    feat: create BU modal

---
+++ app/components/business-units/creation/CreateBUComponent.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import CreateButton from "./CreateButton";
+
+export default function CreateBUComponent({ setIsOpen, refetch }: { setIsOpen: (b: boolean) => void, refetch: () => void}) {
+    const [buName, setBuName] = useState("");
+    const [buDescription, setBuDescription] = useState("");
+    const [buMetadata, setBuMetadata] = useState("");
+    const [defaultForecastedEmission, setDefaultForecastedEmission] = useState(0);
+    const [defaultTarget, setDefaultTarget] = useState(0);
+
+    return (
+        <div className="px-6">
+            <div className="text-left w-full mx-auto mt-8">
+                <div className="mt-1 w-full relative">
+                    <label className="text-left text-neutral-100 pl-1 text-sm leading-tight uppercase">Name</label>
+                    <input 
+                        className={`bg-opacityLight-5 text-left outline-0 border border-opacityLight-10 px-3 py-3 rounded-xl w-full focus:border-neutral-300`} 
+                        type="text" 
+                        value={buName} 
+                        name="buName" 
+                        aria-label="Name" 
+                        placeholder="Enter business unit name"
+                        onChange={(e) => setBuName(e.target.value)} 
+                    />
+                </div>
+                <div className="mt-4 w-full relative">
+                    <label className="text-left text-neutral-100 pl-1 text-sm leading-tight uppercase">Description</label>
+                    <input 
+                        className={`bg-opacityLight-5 text-left outline-0 border border-opacityLight-10 px-3 py-3 rounded-xl w-full focus:border-neutral-300`} 
+                        type="text" 
+                        value={buDescription} 
+                        name="buDescription" 
+                        aria-label="Description" 
+                        placeholder="Enter business unit description"
+                        onChange={(e) => setBuDescription(e.target.value)} 
+                    />
+                </div>
+                <div className="mt-4 w-full relative">
+                    <label className="text-left text-neutral-100 pl-1 text-sm leading-tight uppercase">Metadata</label>
+                    <input 
+                        className={`bg-opacityLight-5 text-left outline-0 border border-opacityLight-10 px-3 py-3 rounded-xl w-full focus:border-neutral-300`} 
+                        type="text" 
+                        value={buMetadata} 
+                        name="buMetadata" 
+                        aria-label="Metadata" 
+                        placeholder="key-value (eg. type-usine, color-red)"
+                        onChange={(e) => setBuMetadata(e.target.value)} 
+                    />
+                </div>
+                <div className="mt-4 w-full relative">
+                    <label className="text-left text-neutral-100 pl-1 text-sm leading-tight uppercase">Default forecasted emission</label>
+                    <input 
+                        className={`bg-opacityLight-5 text-left outline-0 border border-opacityLight-10 px-3 py-3 rounded-xl w-full focus:border-neutral-300`} 
+                        type="number" 
+                        value={defaultForecastedEmission} 
+                        name="defaultForecastedEmission" 
+                        aria-label="Default forecasted emission" 
+                        placeholder="Enter default forecasted emission"
+                        onChange={(e) => setDefaultForecastedEmission(parseInt(e.target.value))} 
+                    />
+                </div>
+                <div className="mt-4 w-full relative">
+                    <label className="text-left text-neutral-100 pl-1 text-sm leading-tight uppercase">Default target</label>
+                    <input 
+                        className={`bg-opacityLight-5 text-left outline-0 border border-opacityLight-10 px-3 py-3 rounded-xl w-full focus:border-neutral-300`} 
+                        type="number" 
+                        value={defaultTarget} 
+                        name="defaultTarget" 
+                        aria-label="Default target" 
+                        placeholder="Enter default target"
+                        onChange={(e) => setDefaultTarget(parseInt(e.target.value))} 
+                    />
+                </div>
+                <div className="w-full text-right my-8">
+                    <CreateButton
+                        setIsOpen={setIsOpen}
+                        buName={buName}
+                        buDescription={buDescription}
+                        buMetadata={buMetadata}
+                        defaultForecastedEmission={defaultForecastedEmission}
+                        defaultTarget={defaultTarget}
+                        hasError={false}
+                        refetch={refetch}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
