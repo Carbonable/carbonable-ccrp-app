9c044c7e1d18d92bdba7bb0de29483eea747e3d9
François Schuers <francois.schuers@gmail.com>
2023-12-13 10:23:38 +0100

    fix: image size in gallery

--- app/components/project/overview/ImagesGallery.tsx
+++ app/components/project/overview/ImagesGallery.tsx
@@ -7,8 +7,8 @@ export default function ImageGallery({images, videos}: {images: any[], videos: a
     return (
         <div className="mb-12 grid grid-cols-1 md:grid-cols-3 gap-1">
             {images.map((image, index) => (
-                <div key={`image_${index}`} className="outline-none">
-                    <img src={urlFor(image).width(500).url()} alt={`image_${index}`} className={`w-full object-cover outline-none` } />
+                <div key={`image_${index}`} className="outline-none w-full h-full overflow-hidden">
+                    <img src={urlFor(image).width(500).url()} alt={`image_${index}`} className={`w-full h-full object-cover outline-none` } />
                 </div>
             ))}
             {videos && videos.map((video, index) => (
