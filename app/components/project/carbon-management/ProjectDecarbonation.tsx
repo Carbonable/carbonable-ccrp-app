d667c3c531d2e44b469caa586533fa84efe43f74
François Schuers <francois.schuers@gmail.com>
2024-01-09 11:18:14 +0100

    fix: #29

--- app/components/project/carbon-management/ProjectDecarbonation.tsx
+++ app/components/project/carbon-management/ProjectDecarbonation.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
-import ProjectDecarbonationComponent from "~/components/common/net-zero/ProjectDecarbonation";
 import { NET_ZERO_PLANNING } from "~/graphql/queries/net-zero";
+import ProjectDecarbonationComponent from "./ProjectDecarbonationComponent";
 
 export default function ProjectDecarbonation({ isFullScreen, projectId }: { isFullScreen: boolean, projectId: string }) {
     const { loading, error, data, refetch } = useQuery(NET_ZERO_PLANNING, {
